af1dd9fb300b4290dba536638a148d11c4d44682
Bensadia Omar <bensadia.omar@gmail.com>
2023-04-21 10:56:14 +0200

    score qui marche

--- scriptsTech.js
+++ scriptsTech.js
@@ -101,8 +101,8 @@ rep[i].addEventListener('click',function(event){
         event.preventDefault(); 
         if (rep2[i].innerHTML === goodq2.innerHTML){
             goodq2.style.backgroundColor = 'green';
-            scoreFinal2 = scoreFinal + 1;
-            score.innerHTML = scoreFinal2 ;
+            scoreFinal = scoreFinal + 1;
+            score.innerHTML = scoreFinal ;
             bq2.addEventListener('click', function(){
                 question2.style.display = 'none';
                 question3.style.display = 'initial';
@@ -139,8 +139,8 @@ for(let i = 0; i < rep3.length; i++){
         if (rep3[i].innerHTML === goodq3.innerHTML){
             console.log('bonne réponse!!');
             goodq3.style.backgroundColor = 'green';
-            scoreFinal3 = scoreFinal2 + 1;
-            score.innerHTML = scoreFinal3 ;
+            scoreFinal = scoreFinal + 1;
+            score.innerHTML = scoreFinal ;
             bq3.addEventListener('click', function(){
                 question3.style.display = 'none';
                 question4.style.display = 'initial';
@@ -165,8 +165,8 @@ for(let i = 0; i < rep3.length; i++){
         rep4[i].addEventListener('click',function(){
             if (rep4[i].innerHTML === goodq4.innerHTML){
                 goodq4.style.backgroundColor = 'green';
-                scoreFinal4 = scoreFinal3 + 1;
-            score.innerHTML = scoreFinal4 ;
+                scoreFinal = scoreFinal + 1;
+            score.innerHTML = scoreFinal ;
                 bq4.addEventListener('click', function(){
                     question4.style.display = 'none';
                     question5.style.display = 'initial';
@@ -194,8 +194,8 @@ for(let i = 0; i < rep3.length; i++){
         rep5[i].addEventListener('click',function(){
             if (rep5[i].innerHTML === goodq5.innerHTML){
                 goodq5.style.backgroundColor = 'green';
-                scoreFinal5 = scoreFinal4 + 1;
-            score.innerHTML = scoreFinal5 ;
+                scoreFinal = scoreFinal + 1;
+            score.innerHTML = scoreFinal ;
                 bq5.addEventListener('click', function(){
                     question5.style.display = 'none';
                     resultatFinal.style.display = 'initial';
@@ -215,4 +215,4 @@ for(let i = 0; i < rep3.length; i++){
 
           }
 
-          score.innerHTML = scoreFinal + scoreFinal2;
+          score.innerHTML = scoreFinal ;
